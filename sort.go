5bf458e0587426065208bc7c522042b5bf8890e4
Anatolii Kochurkov <kochurkov@gmail.com>
2020-12-02 14:56:18 +0200

    Add slice sort

---
+++ sort.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+
+	xi := []int{56, 67, 98, 3, 1, 0, -5}
+	xi = append(xi, -100)
+
+	xs := []string{"Формула", "Бабушка", "Ю", "І", "А", "D", "Z", "I"}
+
+	fmt.Println(xi)
+	fmt.Println(xs)
+
+	sort.Ints(xi)
+	sort.Strings(xs)
+
+	fmt.Println(xi)
+	fmt.Println(xs)
+
+}
