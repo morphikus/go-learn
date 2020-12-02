c6362d393184ee901c51834df41dfd5dce81caf1
Anatolii Kochurkov <kochurkov@gmail.com>
2020-12-02 15:01:37 +0200

    Add 99 to []string

--- sort.go
+++ sort.go
@@ -10,7 +10,7 @@ func main() {
 	xi := []int{56, 67, 98, 3, 1, 0, -5}
 	xi = append(xi, -100)
 
-	xs := []string{"Формула", "Бабушка", "Ю", "І", "А", "D", "Z", "I"}
+	xs := []string{"Формула", "Бабушка", "Ю", "І", "99", "А", "D", "Z", "I"}
 
 	fmt.Println(xi)
 	fmt.Println(xs)
