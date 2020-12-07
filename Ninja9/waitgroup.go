48f0f437f3536b4af29440ac8d1040117bc25a47
Anatolii Kochurkov <kochurkov@gmail.com>
2020-12-07 22:46:02 +0200

    test

--- Ninja9/waitgroup.go
+++ Ninja9/waitgroup.go
@@ -1,1 +1,43 @@
-package Ninja9
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+
+	fmt.Println("OS\t\t", runtime.GOOS)
+	fmt.Println("ARCH\t\t", runtime.GOARCH)
+	fmt.Println("CPUs\t\t", runtime.NumCPU())
+	fmt.Println("Goroutines\t", runtime.NumGoroutine())
+
+	wg.Add(1)
+	go foo()
+
+	wg.Add(1)
+	go bar()
+
+	fmt.Println("Goroutines\t", runtime.NumGoroutine())
+
+	wg.Wait()
+
+}
+
+func foo() {
+	defer wg.Done()
+	for i := 0; i < 10; i++ {
+		fmt.Println("foo:", i)
+	}
+
+}
+
+func bar() {
+	defer wg.Done()
+	for i := 0; i < 10; i++ {
+		fmt.Println("bar:", i)
+	}
+}
