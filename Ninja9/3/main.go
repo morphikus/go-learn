1b906af9a76bdcb6d50b2f13ab956b2aa01c4c4c
Anatolii Kochurkov <kochurkov@gmail.com>
2020-12-08 12:27:53 +0200

    ninja 3 my point of view

--- Ninja9/3/main.go
+++ Ninja9/3/main.go
@@ -1,1 +1,44 @@
-package _
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var counter int
+var wg sync.WaitGroup
+var mu sync.Mutex
+
+func main() {
+
+	n := 100
+	counter = 0
+
+	wg.Add(n)
+
+	for i := 0; i < n; i++ {
+
+		go func() {
+
+			mu.Lock()
+			defer mu.Unlock()
+
+			defer wg.Done()
+
+			c := counter
+			c++
+			counter = c
+
+			runtime.Gosched()
+
+		}()
+
+	}
+
+	wg.Wait()
+
+	fmt.Println("Counter", counter)
+	fmt.Println("Exit")
+
+}
