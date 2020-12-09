863004ea8ba940ab256a3741e9385e6b5a1630a6
Anatolii Kochurkov <kochurkov@gmail.com>
2020-12-09 22:56:23 +0200

    channel learn

---
+++ channels/channel-block/01/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	c := make(chan int)
+
+	go foo(c)
+	bar(c)
+
+	fmt.Println("Exit")
+
+}
+
+func foo(c chan<- int)  {
+	c <- 39
+}
+
+func bar(c <-chan int)  {
+	fmt.Println(<-c)
+}
