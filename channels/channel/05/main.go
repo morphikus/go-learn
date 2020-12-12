720ef6443977511b131ed511375bd87d29e9ea9a
Anatolii Kochurkov <kochurkov@gmail.com>
2020-12-12 23:14:13 +0200

    ok channel

---
+++ channels/channel/05/main.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func  main()  {
+
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	quit := make(chan bool)
+
+	go send(ch1, ch2, quit)
+
+	receive(ch1, ch2, quit)
+
+	fmt.Println("about to exit")
+
+}
+
+func receive(ch1, ch2 <-chan int, quit <-chan bool)  {
+
+	for{
+		select {
+			case v := <- ch1:
+				fmt.Println("from ch1 channel", v)
+			case v := <- ch2:
+				fmt.Println("from ch2 channel", v)
+			case v, ok := <-quit:
+
+				if !ok{
+					fmt.Println("from comma ok bit", v, ok)
+					return
+				} else{
+					fmt.Println("from comma ok bit", v)
+				}
+
+		}
+
+	}
+
+}
+
+func send(ch1, ch2 chan<- int, quit chan<- bool)  {
+
+	for i := 0; i < 10; i++{
+		if i % 2 == 0 {
+			ch1 <- i
+		} else {
+			ch2 <- i
+		}
+	}
+	//quit <- true
+	close(quit)
+
+}
