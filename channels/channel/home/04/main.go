5df06a619f75d88013961bc14f9d50f3e8b79a26
Anatolii Kochurkov <kochurkov@gmail.com>
2020-12-14 21:25:59 +0200

    home 10 04

--- channels/channel/home/04/main.go
+++ channels/channel/home/04/main.go
@@ -5,14 +5,14 @@ import (
 )
 
 func main() {
-	//cs := make(chan<- int)
-	cs := make(chan int)
+	//cr := make(<-chan int)
+	cr := make(chan int)
 
 	go func() {
-		cs <- 42
+		cr <- 42
 	}()
-	fmt.Println(<-cs)
+	fmt.Println(<-cr)
 
 	fmt.Printf("------\n")
-	fmt.Printf("cs\t%T\n", cs)
+	fmt.Printf("cr\t%T\n", cr)
 }
